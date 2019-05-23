cb3d5c3420e4f2c08ddf6df808345788b22d3444
thitiwat thongbor <thitiwat.thongbor@gmail.com>
2019-05-23 14:09:54 +0700

    add dotenv

--- src/database/index.js
+++ src/database/index.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 const apis = {
-  apiKey: "AIzaSyBs7oox646evUZzHxiD69UCON2xWGaPj18",
-  authDomain: "spa-final-exam6384.firebaseapp.com",
-  databaseURL: "https://spa-final-exam6384.firebaseio.com",
-  projectId: "spa-final-exam6384",
-  storageBucket: "spa-final-exam6384.appspot.com",
-  messagingSenderId: "942365828813",
-  appId: "1:942365828813:web:f7c8f77f9ae53429"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 firebase.initializeApp(apis);
